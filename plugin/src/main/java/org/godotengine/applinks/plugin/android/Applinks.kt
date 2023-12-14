0264ecb7185b6385247c294fe0bdb379b3543cb6
FoolLin <theworldrudo549@gmail.com>
2023-12-14 19:08:02 +0700

    Change class name

--- plugin/src/main/java/org/godotengine/applinks/plugin/android/Applinks.kt
+++ plugin/src/main/java/org/godotengine/applinks/plugin/android/Applinks.kt
@@ -1,14 +1,12 @@
 // TODO: Update to match your plugin's package name.
 package org.godotengine.applinks.plugin.android
 
-import android.util.Log
-import android.widget.Toast
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.UsedByGodot
 import org.godotengine.godot.GodotFragment
 
-class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
+class Applinks(godot: Godot): GodotPlugin(godot) {
 
     override fun getPluginName() = BuildConfig.GODOT_PLUGIN_NAME
 
